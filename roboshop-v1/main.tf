42c83a9e701bcbf21f9e881d932139410e7a26a4
lenovo <omkar.reddy211@gmailcom>
2024-10-25 15:33:58 +0530

    payment script

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,4 +1,8 @@
     resource "aws_instance" "frontend" {
         ami = "ami-09c813fb71547fc4f"
         instance_type= "t3.small"
+        vpc_security_source_id= ["sg-0af16956e6a252f7a"]
+        tags = {
+            Name = "frontend"
+            }
         }
