d04ea80fc522e26f37c198472925cdaa7f9ec986
lenovo <omkar.reddy211@gmailcom>
2024-10-26 13:37:50 +0530

    payment script

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,8 +1,16 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-0af16956e6a252f7a"]
-  tags = {
-    Name = "frontend"
-  }
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_id = [ "sg-0af16956e6a252f7a" ]
+    tags = {
+        name = "frontend"
+        }
+    }
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z0568841399D7VVAXAQ1A"
+  name    = "frontend-dev.rdevopsb72.shop"
+  type    = "A"
+  ttl     = "30"
+  records = ["aws_instance.frontend.private_ip"]
 }
