09e99f0564294b198689bd856c8a95def924941a
lenovo <omkar.reddy211@gmailcom>
2024-11-01 17:49:48 +0530

    payment script

---
+++ roboshop_count_loop/main.tf
@@ -0,0 +1,24 @@
+variable "instances" {
+  default = [
+    "frontend",
+    "cart",
+    "catalogue",
+    "user",
+    "shipping",
+    "payment",
+    "mysql",
+    "mongodb",
+    "rabbitmq",
+    "redis"
+  ]
+}
+
+resource "aws_instance" "frontend" {
+    count= length(var.instances)
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+    vpc_security_group_ids = [ "sg-05180aaee83558bdc" ]
+    tags = {
+        Name = var.instances[count.index]
+        }
+    }
