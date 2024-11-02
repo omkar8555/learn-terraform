b949524f988cc5d218e1b652fbd9e1a2703de85d
lenovo <omkar.reddy211@gmailcom>
2024-11-02 19:13:02 +0530

    payment script

--- 6.modules/ec2/main.tf
+++ 6.modules/ec2/main.tf
@@ -1,9 +1,10 @@
-resource "aws_route53_record" "record" {
-  zone_id = "Z08947163LH0CXG3JRYK4"
-  name    = "${var.component_name}-dev.rdevopsb72.shop"
-  type    = "A"
-  ttl     = "30"
-  records = [aws_instance.instance.private_ip]
+resource "aws_instance" "instance" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-05180aaee83558bdc"]
+  tags = {
+    Name = var.component_name
+  }
 }
 
 variable "instance_name" {}
