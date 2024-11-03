314ca065dd03ebc061655f67205a112270212534
lenovo <omkar.reddy211@gmailcom>
2024-11-03 21:38:01 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -1,15 +1,21 @@
-resource "aws_instance" "frontend" {
-    ami = "ami-09c813fb71547fc4f"
-    instance_type = "t3.small"
-    vpc_security_group_ids = ["sg-05180aaee83558bdc"]
-    tags ={
-        Name = "frontend"
-        }
+variable "x" {
+    default = "omkar"
     }
-resource "aws_route53_record" "record" {
-    zone_id = "Z08947163LH0CXG3JRYK4"
-    name = "frontend-dev.rdevopsb72.shop"
-    type = "A"
-    ttl = "30"
-    records =[aws_instance.frontend.private_ip]
+output "name" {
+    value = var.x
+    }
+
+output "name1" {
+    value = "${var.x} reddy"
+    }
+
+variable "A" {
+    default = "omkar"
+    }
+variable "B" {
+    default = "reddy"
+    }
+
+output "name2" {
+    value =  "${var.A} ${var.B}"
     }
