51e4a1f0551439ff4cc31bd3d612ec6b9c33eaa5
lenovo <omkar.reddy211@gmailcom>
2024-11-03 17:33:06 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -5,7 +5,7 @@
          }
      }
 
- resource "aws_instance" "A" {
+ resource "aws_instance" "instance" {
      count = length(var.instance)
      ami = "ami-09c813fb71547fc4f"
      instance_type = "t3.small"
