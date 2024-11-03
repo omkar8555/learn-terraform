539e9185725af1d44134da6c5bcb3edddf0d446f
lenovo <omkar.reddy211@gmailcom>
2024-11-03 16:44:48 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -1,23 +1,3 @@
-variable "map" {
-    default = {
-       aws = {
-           trainer ="a"
-           duration = 30
-           }
-       devops = {
-           trainer ="b"
-           duration = 30
-           }
-    }
-}
-
-output "aws_trainer" {
-    value= var.map["aws"].trainer
-}
-
-
-variable "trainer_name"  {}
-
-output "trainer_name" {
-    value = var.trainer_name
-    }
+ resource "null_resource" "demo" {
+     count =10
+     }
