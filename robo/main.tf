e96b74dce7bb4720eb07d38f045fdb1f1bdf9b2f
lenovo <omkar.reddy211@gmailcom>
2024-11-03 18:54:16 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -7,7 +7,7 @@
  }
 
  resource "aws_instance" "instance" {
-   for_each = var.instance
+   for_each = var.instances
    ami           = "ami-09c813fb71547fc4f"
    instance_type = "t3.small"
    vpc_security_group_ids = ["sg-05180aaee83558bdc"]
@@ -17,7 +17,7 @@
  }
 
  resource "aws_route53_record" "record" {
-      for_each = var.instance
+      for_each = var.instances
    zone_id = "Z08947163LH0CXG3JRYK4"
    name    = "${each.key}-dev.rdevopsb72.shop"
    type    = "A"
