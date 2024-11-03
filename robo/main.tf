0fa8a03b5555c12b70ddeb73a997b13451dd48be
lenovo <omkar.reddy211@gmailcom>
2024-11-03 19:14:56 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -1,26 +1,19 @@
- variable "instances" {
-   default = {
-     frontend = {}
-     cart = {}
+resource "aws_instance"  "h" {
+    ami = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = [data.aws_security_groups.sg.id]
+  tags = {
+    Name = gh
+  }
+}
 
-   }
- }
 
- resource "aws_instance" "instance" {
-   for_each = var.instances
-   ami           = "ami-09c813fb71547fc4f"
-   instance_type = "t3.small"
-   vpc_security_group_ids = ["sg-05180aaee83558bdc"]
-   tags = {
-     Name = each.key
-   }
- }
+ data "aws_ami" "ami" {
+   most_recent      = true
+   name_regex       = "RHEL-9-DevOps-Practice"
+   owners           = ["973714476881"]
 
- resource "aws_route53_record" "record" {
-      for_each = var.instances
-   zone_id = "Z08947163LH0CXG3JRYK4"
-   name    = "${each.key}-dev.rdevopsb72.shop"
-   type    = "A"
-   ttl     = "30"
-   records = [aws_instance.instance[each.key].private_ip]
- }
+data "aws_security_groups" "id" {
+  filter {
+    name   = "allow-all"
+  }
