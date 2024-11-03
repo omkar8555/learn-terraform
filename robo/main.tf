e4fc5604418a4764cc28c6183a14a914dcb7b24e
lenovo <omkar.reddy211@gmailcom>
2024-11-03 15:34:16 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -14,3 +14,31 @@ variable "map" {
 output "map" {
     value = var.map["aws"]["trainer"]
     }
+
+variable "map1" {
+  default = {
+    aws = {
+      trainer = "John"
+      duration = 30
+    }
+    devops = {
+      trainer = "Steve"
+      duration = 20
+    }
+  }
+}
+
+output "list1_1" {
+  value = var.list1[2]
+}
+
+output "map1_aws_trainer" {
+  value = var.map1["aws"]["trainer"]
+  #value = var.map1["aws"].trainer
+}
+
+# Declare a empty variable and we can get the value from CLI, Unlike ansible or shell we must and should declare a variable in order to use it.
+variable "trainer_name" {}
+output "trainer_name" {
+  value = var.trainer_name
+}
