e7de70e34a780758920637c9998518b06a8fa072
lenovo <omkar.reddy211@gmailcom>
2024-11-03 21:18:39 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "frontend" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.small"
-    vpc_security_group_ids = "sg-05180aaee83558bdc"
+    vpc_security_group_ids = ["sg-05180aaee83558bdc"]
     tags ={
         Name = "frontend"
         }
     }
 resource "aws_route53_record" "record" {
     zone_id = "Z08947163LH0CXG3JRYK4"
-    name = fronend-dev.rdevopsb72.shop
+    name = "frontend-dev.rdevopsb72.shop"
     type = "A"
     ttl = "30"
-    records =[aws_instance.instance.private_ip]
+    records =[aws_instance.frontend.private_ip]
     }
