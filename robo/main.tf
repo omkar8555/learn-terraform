d3381fa7ede47179c3608dbe0c712143258aa615
lenovo <omkar.reddy211@gmailcom>
2024-11-03 19:19:32 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -12,6 +12,7 @@ resource "aws_instance"  "h" {
    most_recent      = true
    name_regex       = "RHEL-9-DevOps-Practice"
    owners           = ["973714476881"]
+   }
 
 data "aws_security_group" "sg" {
   name = "allow-all"
