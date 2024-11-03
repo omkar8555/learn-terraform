2074795e38870bfc2b6d2dc35ed5c058412aa66c
lenovo <omkar.reddy211@gmailcom>
2024-11-03 17:37:07 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -1,4 +1,4 @@
- variable "instance" {
+ variable "instances" {
      default = [
          "frontend",
         " mongodb"]
@@ -7,16 +7,16 @@
      }
 
  resource "aws_instance" "instance" {
-     count = length(var.instance)
+     count = length(var.instances)
      ami = "ami-09c813fb71547fc4f"
      instance_type = "t3.small"
       vpc_security_group_ids =["sg-05180aaee83558bdc"]
      tags ={
-         Name = var.instance[count.index]
+         Name = var.instances[count.index]
          }
      }
  resource "aws_route53_record" "record" {
-     count = length(var.instance)
+     count = length(var.instances)
     zone_id = "Z08947163LH0CXG3JRYK4"
      name = "${var.instance}-dev.rdevopsb72.shop"
      type = "A"
