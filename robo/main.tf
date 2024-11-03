1cf75062e80c52b9cd0b9ff7225b5db6ee544ed3
lenovo <omkar.reddy211@gmailcom>
2024-11-03 17:24:33 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -9,7 +9,7 @@
      count = length(var.instance)
      ami = "ami-09c813fb71547fc4f"
      instance_type= "t3.small"
-     vpc_security_group_id =["sg-05180aaee83558bdc"]
+      vpc_security_group_ids =["sg-05180aaee83558bdc"]
      tags ={
          Name=var.instance[count.index]
          }
