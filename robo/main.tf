3faa4499df39a3d95e9c7278006d28264ab0d3f8
lenovo <omkar.reddy211@gmailcom>
2024-11-04 11:31:24 +0530

    payment script

--- robo/main.tf
+++ robo/main.tf
@@ -1,11 +1,19 @@
-variable "fruits" {
-    default = [
-"apple",
-    "banana",
-    "mango"
-    ]
-}
+data "aws_ami" "ami" {
+    most recent = true
+    nam_regex = "RHEL-9-DevOps-Practice"
+    owner = ["973714476881"]
 
-resource "null_resource" "fruits" {
-    for_each = var.fruits
+    }
+
+data "aws_security_group" "sg" {
+    name "allow-all"
+    }
+
+resource "aws_instance" "test" {
+    ami = "data.aws_ami.ami.id"
+    instance_type = "t3.small"
+    vpc_aws_security_group-ids = ["aws_security_group.sg.id"]
+    tags = {
+        Name = "test"
+        }
     }
