86d13b6cbb754164c419e27d848952da95842418
lenovo <omkar.reddy211@gmailcom>
2024-11-06 15:44:44 +0530

    payment script

--- 9.vault/main.tf
+++ 9.vault/main.tf
@@ -10,7 +10,16 @@ provider "vault" {
     path = "test/my_credentials"
   }
 
+  # data "vault_kv_secret_v2" "example" {
+  #   name  = "my_credentials"
+  #   mount = "test"
+  # }
+
   resource "local_file" "foo" {
-    content  = "data.vault_generic_secret.example.data["password"]"
+    content  = data.vault_generic_secret.example.data["password"]
     filename = "/tmp/secret"
   }
+
+  # output "json" {
+  #   value = data.vault_kv_secret_v2.example.data_json
+  # }
