17c37eb09310187811522814aab00f121945e44f
lenovo <omkar.reddy211@gmailcom>
2024-11-07 14:00:10 +0530

    payment script

--- 6.modules/main.tf
+++ 6.modules/main.tf
@@ -9,7 +9,7 @@ module "ec2" {
     count = length(var.instances)
     source = "./ec2"
     component_name = var.instances[count.index]
-    ip_address = module.ec2[count.index].ip_address
+    ip_address = module[count.index].ip_address
 
     }
 
