d2ef3f5900611e071a66f9f71b228df6b6db6c30
lenovo <omkar.reddy211@gmailcom>
2024-11-07 11:42:57 +0530

    payment script

--- 10.multi-env-code-strugture1/modules/main.tf
+++ 10.multi-env-code-strugture1/modules/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instace" {
     ami           = "ami-09c813fb71547fc4f"
     instance_type = "t3.small"
-    vpc_secuirity_group_id = ["sg-05180aaee83558bdc"]
+    vpc_secuirity_group_ids = ["sg-05180aaee83558bdc"]
      tags = {
          Name = "test-${var.env}"
          }
