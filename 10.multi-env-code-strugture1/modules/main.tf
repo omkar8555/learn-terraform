7dddab7f4be41d29e61b2eee4df79cc0a64fddc1
lenovo <omkar.reddy211@gmailcom>
2024-11-07 12:26:20 +0530

    payment script

--- 10.multi-env-code-strugture1/modules/main.tf
+++ 10.multi-env-code-strugture1/modules/main.tf
@@ -13,5 +13,5 @@ resource "aws_route53_record" "record" {
   name    = "test-${var.env}.rdevopsb72.shop"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.instance.public_ip]
+  records = ["aws_instance.instance.public_ip"]
 }
