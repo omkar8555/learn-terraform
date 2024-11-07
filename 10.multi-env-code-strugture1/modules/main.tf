86aa8c33a390fadbd71cbb5f946a613de0836c18
lenovo <omkar.reddy211@gmailcom>
2024-11-07 13:05:00 +0530

    payment script

--- 10.multi-env-code-strugture1/modules/main.tf
+++ 10.multi-env-code-strugture1/modules/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "instace" {
+resource "aws_instance" "instance" {
     ami           = "ami-09c813fb71547fc4f"
     instance_type = "t3.small"
     vpc_security_group_ids = ["sg-05180aaee83558bdc"]
@@ -8,3 +8,10 @@ resource "aws_instance" "instace" {
      }
 variable "env" {}
 
+resource "aws_route53_record" "record" {
+  zone_id = "Z08947163LH0CXG3JRYK4"
+  name    ="test-${var.env}.rdevopsb72.shop"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.instance.private_ip]
+}
