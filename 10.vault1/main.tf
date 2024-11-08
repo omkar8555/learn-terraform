bb9d8a9ce4d81373bfdb484618963ad84c5844a8
lenovo <omkar.reddy211@gmailcom>
2024-11-08 14:03:08 +0530

    payment script

--- 10.vault1/main.tf
+++ 10.vault1/main.tf
@@ -8,12 +8,11 @@ provider "vault" {
 
 
 data "vault_generic_secret" "example" {
-  path = "test/my_credentials"
-}
-
+    path = "test/my_credentials"
+  }
 
 
   resource "local_file" "foo" {
-    content  = data.vault_generic_secret.example.data["password"]
-    filename = "tmp/secret"
-  }
+      content  = data.vault_generic_secret.example.data["password"]
+      filename = "/tmp/secret"
+    }
