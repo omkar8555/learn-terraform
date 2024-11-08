36d0d7b398ffb3ae3578a0f047d1452f670a8485
lenovo <omkar.reddy211@gmailcom>
2024-11-08 13:57:10 +0530

    payment script

---
+++ 10.vault1/main.tf
@@ -0,0 +1,19 @@
+provider "vault" {
+  address = "http://172.31.45.188:8200"
+  token   = var.vault_token
+  skip_tls_verify = true
+  }
+
+  variable "vault_token" {}
+
+
+data "vault_generic_secret" "example" {
+  path = "test/my_credentials"
+}
+
+
+
+  resource "local_file" "foo" {
+    content  = "data.vault_generic_secret.example.data["password"]"
+    filename = "tmp/secret"
+  }
